0a3a47f98972a578b19a26aa9df263d84e8af3de
Mr.Qin <70084111+meystack@users.noreply.github.com>
2022-04-09 23:06:16 +0800

    配置升级

--- config/plugin.php
+++ config/plugin.php
@@ -2,18 +2,8 @@
 return [
   'autoload' => false,
   'hooks' => [
-    'appInit' => [
-      0 => 'demo',
-    ],
-    'user_sidenav_after' => [
-      0 => 'demo',
-    ]
   ],
-  'router' => [
-    '/demo1$' => 'demo/index/index',
-    '/demo2$' => 'demo/index/list',
-    '/demo3$' => 'demo/index/query',
-  ],
+  'router' => [],
   'priority' => [
   ],
 ];
